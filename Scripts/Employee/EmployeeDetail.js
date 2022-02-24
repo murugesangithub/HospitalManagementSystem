27564295556e32d4d1bfdfe795a59c97b0a73b94
murugean p <murugesanp@live.com>
2022-02-24 15:23:35 +0530

    grid implementation

---
+++ Scripts/Employee/EmployeeDetail.js
@@ -0,0 +1,156 @@
+﻿
+$(document).ready(function () {
+
+    var grid = "#jqEmployeeDetailGrid";
+    var gridpager = "#jqEmployeeDetailGridPager";
+
+    var bodyElem = $('.content-wrapper');
+    new ResizeSensor(bodyElem, function () {
+        var bodyElemWidth = Math.round($('.content-wrapper').width());
+        var newGridWidth = bodyElemWidth - 25;
+        $(grid).jqGrid("setGridWidth", newGridWidth, true);
+        $('.ui-jqgrid-bdiv').css('overflow', 'hidden');
+    });
+
+
+    $(grid).jqGrid({
+        url: relativepath + "Employee/GeEmployeeList",
+        mtype: "POST",
+        styleUI: 'Bootstrap',
+        datatype: "json",
+        height: 'auto',
+        jsonReader: jsonreader,
+        colModel: [
+
+            //{ label: 'UserDetailId', name: 'UserDetailId', key: true, width: 100, hidden: true, },
+            //{ label: 'EncryptUserDetailId', name: 'EncryptUserDetailId', width: 100, hidden: true, },
+            //{
+            //    label: 'Profile',
+            //    name: 'ProfileImage',
+            //    width: 50,
+            //    search: false,
+            //    align: 'center',
+            //    formatter: function (cellvalue, options, rowObject) {
+            //        var img = "<img src='/Images/default_profile.jpg' alt='Profile Image' class='rounded-circle' width='36' >"
+            //        if (cellvalue.length > 0) {
+            //            img=  "<img src='" + cellvalue + "' alt='Profile Image' class='rounded-circle' width='36' />";
+            //        }
+            //        return img;
+            //    }
+            //},
+            { label: 'Name', name: 'Name', width: 200, },
+            { label: 'Address', name: 'Address', width: 200, },
+
+        ],
+        rownumbers: true,
+        viewrecords: true,
+        rowNum: 10,
+        rowList: [10, 20, 30, 40, 50],
+        //editurl: relativepath + "Material/MaterialManipulation",
+        pager: gridpager,
+        caption: "User List"
+
+    });
+    $(grid).jqGrid('navGrid', gridpager, { edit: false, add: false, del: true, refresh: true, search: false },
+
+        {},
+        {},
+        {
+            url: relativepath + "UserDetail/UserDeletion",
+            closeOnEscape: true,
+            reloadAfterSubmit: true,
+            closeAfterDel: true,
+            recreateFrom: true,
+
+            delData: {
+                UserDetailId: function () {
+                    var selRowId = $(grid).jqGrid('getGridParam', 'selrow');
+                    var rowData = $(grid).jqGrid("getRowData", selRowId);
+                    return rowData.UserDetailId;
+                }
+            }
+        },
+        {},
+        {}
+    );
+
+    //$(grid).jqGrid('inlineNav', gridpager, {
+    //    addParams: {
+    //        position: 'last',
+    //        addRowParams: {
+    //            keys: true,
+    //            successfunc: function () {
+    //                $(this).trigger("reloadGrid");
+    //            }
+    //        }
+    //    }, editParams: {
+    //        keys: true,
+    //        successfunc: function () {
+    //            $(this).trigger("reloadGrid");
+    //        }
+    //    }
+    //});
+
+    $(grid).jqGrid('navButtonAdd', gridpager,
+        {
+            caption: "", buttonicon: "glyphicon glyphicon-plus", title: "Add new row",
+            onClickButton: function () {
+                window.location = relativepath + "UserDetail/Register";
+            },
+
+        });
+    $(grid).jqGrid('navButtonAdd', gridpager,
+        {
+            caption: "", buttonicon: "glyphicon glyphicon-edit", title: "Edit selected row",
+            onClickButton: function () {
+                var selRowId = $(grid).jqGrid('getGridParam', 'selrow');
+                if (selRowId == null) {
+                    $.jgrid.info_dialog('Warning', 'Please, select row', '', { styleUI: 'Bootstrap' });
+                } else {
+                    var rowData = $(grid).jqGrid("getRowData", selRowId);
+                    window.location = relativepath + "UserDetail/UpdateUserDetail?id=" + rowData.EncryptUserDetailId;
+                }
+            }
+        });
+
+    $(grid).jqGrid('navButtonAdd', gridpager,
+        {
+            caption: "", buttonicon: "glyphicon glyphicon-lock", title: "Reset password selected row",
+            onClickButton: function () {
+                var selRowId = $(grid).jqGrid('getGridParam', 'selrow');
+                if (selRowId == null) {
+                    $.jgrid.info_dialog('Warning', 'Please, select row', '', { styleUI: 'Bootstrap' });
+                } else {
+                    var rowData = $(grid).jqGrid("getRowData", selRowId);
+                    window.location = relativepath + "UserDetail/ResetPassword?id=" + rowData.EncryptUserDetailId;
+                }
+            }
+        });
+
+    $(grid).jqGrid('filterToolbar', {
+        stringResult: true,
+        searchOnEnter: false,
+        defaultSearch: "cn",
+        beforeSearch: function () {
+            modifySearchingFilter.call(this, ' ');
+        }
+    });
+
+    $(document).on("click", "#btnsave", function () {
+
+
+        $.ajax({
+            url: relativepath + '/Material/MaterialManipulation',
+            type: "POST",
+            data: fileData,
+            success: function (result) {
+              //  alert(result);
+            },
+            error: function (err) {
+                Notify_Validation(err.statusText);
+            }
+        });
+
+    });
+});
+
---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,65 @@
+﻿using HospitalManagementSystem.Filter;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace HospitalManagementSystem.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Fist Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Required]
+        [Display(Name = "Role")]
+        public List<System.Web.Mvc.SelectListItem> RoleList { get; set; } = new List<System.Web.Mvc.SelectListItem>();
+        public int Role { get; set; }
+        public bool IsActive { get; set; }
+        public int UserDetailId { get; set; }
+        public string RoleDesc { get; set; }
+        [Display(Name = "Profile Image")]
+        [AllowedFileExtension(".jpg", ".png", ".gif", ".jpeg", ".jfif")]
+        public HttpPostedFileBase ImageFileUpload { get; set; }
+        public string ProfileImage { get; set; } = string.Empty;
+        public string EncryptUserDetailId { get; set; }
+    }
+}
