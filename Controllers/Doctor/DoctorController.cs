d98801ed2d37adc61907623b6f4f6815f4040adc
murugesangithub <murugesan.net@gmail.com>
2022-03-03 17:58:22 +0530

    doctor controller

---
+++ Controllers/Doctor/DoctorController.cs
@@ -0,0 +1,111 @@
+﻿using HospitalManagementSystem.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalManagementSystem.Controllers.Doctor
+{
+    public class DoctorController : Controller
+    {
+        public List<SelectListItem> StateList { get; private set; }
+
+        // GET: Doctor
+        public ActionResult Add()
+        {
+            var viewmodel = new DoctorViewModel();
+
+            viewmodel.GenderList = GetGenderList();
+            viewmodel.RoleList = GetRoleList();
+            viewmodel.StateList = GetStateList();
+            viewmodel.CityList = GetCityList();
+
+
+            return View(viewmodel);
+
+        }
+
+
+        private List<SelectListItem> GetGenderList()
+        {
+            var genderList = new List<SelectListItem>()
+            {
+                new SelectListItem(){ Text="Male", Value="M"},
+                new SelectListItem(){ Text="FeMale", Value="F"},
+                new SelectListItem(){ Text="Others", Value="O"},
+
+            };
+            return genderList;
+        }
+
+
+        private List<SelectListItem> GetRoleList()
+        {
+            var RoleList = new List<SelectListItem>()
+            {
+                new SelectListItem(){ Text="Please Select a Specialist:", Value="Speicalist"},
+                new SelectListItem(){ Text="Anaesthesia", Value="Ana"},
+                new SelectListItem(){ Text="Andrology", Value="Andro"},
+                new SelectListItem(){ Text="Ayurvedic", Value="Ayur"},
+                new SelectListItem(){ Text="Cardiologist", Value="Card"},
+                new SelectListItem(){ Text="Densit", Value="Den"},
+                new SelectListItem(){ Text="Dermatologist", Value="Derma"},
+                new SelectListItem(){ Text="Dietician", Value="Diet"},
+                new SelectListItem(){ Text="Endocrinologist", Value="Endo"},
+                new SelectListItem(){ Text="ENT", Value="E"},
+                new SelectListItem(){ Text="Medicine", Value="Medi"},
+                new SelectListItem(){ Text="Nephrologist", Value="Nep"},
+                new SelectListItem(){ Text="Homoeopathy", Value="Hemo"},
+                new SelectListItem(){ Text="Neurologist", Value="Neuro"},
+
+            };
+            return RoleList;
+        }
+
+
+
+        private List<SelectListItem> GetStateList()
+        {
+            var StateList = new List<SelectListItem>()
+            {
+                new SelectListItem(){ Text="Please Select a State:", Value="State"},
+                new SelectListItem(){ Text="Arunachal Pradesh", Value="Aruna"},
+                new SelectListItem(){ Text="Assam", Value="A"},
+                new SelectListItem(){ Text="Bihar", Value="Bi"},
+                new SelectListItem(){ Text="Delhi", Value="Del"},
+                new SelectListItem(){ Text="Goa", Value="G"},
+                new SelectListItem(){ Text="Gujarat", Value="Guj"},
+                new SelectListItem(){ Text="Haryana", Value="Har"},
+                new SelectListItem(){ Text="Himachal Pradesh", Value="Him"},
+                new SelectListItem(){ Text="Jammu and Kashmir", Value="Jam"},
+                new SelectListItem(){ Text="Jharkhand", Value="Jar"},
+                new SelectListItem(){ Text="Karnataka", Value="Karna"},
+                new SelectListItem(){ Text="Kerala", Value="Ker"},
+                new SelectListItem(){ Text="Maharashtra", Value="Mahara"},
+
+            };
+            return StateList;
+        }
+
+
+
+        private List<SelectListItem> GetCityList()
+        {
+            var CityList = new List<SelectListItem>()
+            {
+                new SelectListItem(){ Text="Please Select a City:", Value="State"},
+                new SelectListItem(){ Text="Karad", Value="Kar"},
+                new SelectListItem(){ Text="Mumbai", Value="Mum"},
+                new SelectListItem(){ Text="Nagpur", Value="Nag"},
+                new SelectListItem(){ Text="Nashik", Value="Nas"},
+                new SelectListItem(){ Text="Navi Mumbai", Value="Navi"},
+                new SelectListItem(){ Text="Pune", Value="Pun"},
+                new SelectListItem(){ Text="Thane", Value="Tha"},
+               
+            };
+            return CityList;
+        }
+
+    }
+}
---
+++ ViewModel/DoctorViewModel.cs
@@ -0,0 +1,72 @@
+﻿using HospitalManagementSystem.Filter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using HospitalManagementSystem;
+namespace HospitalManagementSystem.ViewModel
+{
+    public class DoctorViewModel
+    {
+
+        [Required]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Mobile No")]
+        public string MobileNo { get; set; }
+
+        [Required]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Specialist in")]
+        public List<System.Web.Mvc.SelectListItem> RoleList { get; set; } = new List<System.Web.Mvc.SelectListItem>();
+        public int Role { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public List<System.Web.Mvc.SelectListItem> GenderList { get; set; } = new List<System.Web.Mvc.SelectListItem>();
+        public int Gender { get; set; }
+
+
+        [Required]
+        [Display(Name = "State")]
+        public List<System.Web.Mvc.SelectListItem> StateList { get; set; } = new List<System.Web.Mvc.SelectListItem>();
+        public int State { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public List<System.Web.Mvc.SelectListItem> CityList { get; set; } = new List<System.Web.Mvc.SelectListItem>();
+        public int City { get; set; }
+
+    }
+}
