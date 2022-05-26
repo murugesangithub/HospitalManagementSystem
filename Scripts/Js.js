4293aec29d7133516af44c344a07e27f0f33449d
murugesangithub <murugesan.net@gmail.com>
2022-05-26 17:14:41 +0530

    Changes

---
+++ Scripts/Js.js
@@ -0,0 +1,16 @@
+﻿////$(document).ready(function () {
+////    // Add smooth scrolling to all links
+////    $("#Appointment").on('click', function (event) {
+////        if (this.hash !== "") {
+////            event.preventDefault();
+////            // Store hash
+////            var hash = this.hash;
+////            $('html, body').animate({
+////                scrollTop: $(hash).offset().top
+////            }, 1200, function () {
+
+////                window.location.hash = hash;
+////            });
+////        } // End if
+////    });
+////});
